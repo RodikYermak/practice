bcf9ecca8f526c5b6e940a3767560347a675c53f
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2024-03-25 13:47:21 -0700

    leetcode functions practice

--- practice.js
+++ practice.js
@@ -90,9 +90,9 @@ Output: 0
 const numIdenticalPairs = (nums) => {
     let count = 0;
     // console.log(nums);
-    for(let i = 0; i < nums.length - 1; i++){
-        for (let j = i + 1; j < nums.length; j++){
-            if(nums[i] === nums[j]){
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
                 count += 1;
             }
         }
@@ -116,8 +116,102 @@ const numIdenticalPairs = (nums) => {
     // return count;
 };
 
-numIdenticalPairs([1, 2, 3, 1, 1, 3]);
+/*
+There is a programming language with only four operations and one variable X:
+
+++X and X++ increments the value of the variable X by 1.
+--X and X-- decrements the value of the variable X by 1.
+Initially, the value of X is 0.
+
+Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+
+Example 1:
+
+Input: operations = ["--X","X++","X++"]
+Output: 1
+Explanation: The operations are performed as follows:
+Initially, X = 0.
+--X: X is decremented by 1, X =  0 - 1 = -1.
+X++: X is incremented by 1, X = -1 + 1 =  0.
+X++: X is incremented by 1, X =  0 + 1 =  1.
+Example 2:
+
+Input: operations = ["++X","++X","X++"]
+Output: 3
+Explanation: The operations are performed as follows:
+Initially, X = 0.
+++X: X is incremented by 1, X = 0 + 1 = 1.
+++X: X is incremented by 1, X = 1 + 1 = 2.
+X++: X is incremented by 1, X = 2 + 1 = 3.
+Example 3:
+
+Input: operations = ["X++","++X","--X","X--"]
+Output: 0
+Explanation: The operations are performed as follows:
+Initially, X = 0.
+X++: X is incremented by 1, X = 0 + 1 = 1.
+++X: X is incremented by 1, X = 1 + 1 = 2.
+--X: X is decremented by 1, X = 2 - 1 = 1.
+X--: X is decremented by 1, X = 1 - 1 = 0.
+
+
+
+*/
+
+const finalValueAfterOperations = (operations) => {
+    console.log(operations);
+    let result = 0;
+
+    operations.forEach((operation) => {
+        if (operation.includes('-')) {
+            result -= 1;
+        } else {
+            result += 1;
+        }
+    });
+
+    console.log(result);
+    return result;
+};
+
+/*
+
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+Example 1:
+
+Input: nums = [2,5,1,3,4,7], n = 3
+Output: [2,3,5,4,1,7]
+Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+Example 2:
+
+Input: nums = [1,2,3,4,4,3,2,1], n = 4
+Output: [1,4,2,3,3,2,4,1]
+Example 3:
+
+Input: nums = [1,1,2,2], n = 2
+Output: [1,2,1,2]
+
+
+*/
+
+const shuffle = (nums, n) => {
+    console.log(nums, n);
+    const resArr = [];
+
+    for (let i = 0; i < nums.length / 2; i++) {
+        resArr.push(nums[i], nums[i + n]);
+    }
+    console.log(resArr);
+    return resArr;
+};
+
+shuffle([2, 5, 1, 3, 4, 7], 3);
 
 // module.exports = twoSums;
 // module.exports = getConcatenation;
-module.exports = numIdenticalPairs;
+// module.exports = numIdenticalPairs;
+// module.exports = finalValueAfterOperations;
+module.exports = shuffle;
--- practice.test.js
+++ practice.test.js
@@ -1,6 +1,8 @@
 // const twoSums = require('./practice');
 // const getConcatenation = require('./practice');
-const numIdenticalPairs = require('./practice');
+// const numIdenticalPairs = require('./practice');
+// const finalValueAfterOperations = require('./practice');
+const shuffle = require('./practice');
 
 // test('Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.', () => {
 //     expect(twoSums([3, 2, 4], 6)).toEqual([1, 2]);
@@ -13,8 +15,18 @@ const numIdenticalPairs = require('./practice');
 //     expect(getConcatenation([1, 3, 2, 1])).toEqual([1, 3, 2, 1, 1, 3, 2, 1]);
 // });
 
-test('Given an array of integers nums, return the number of good pairs', () => {
-    expect(numIdenticalPairs([1,2,3,1,1,3])).toEqual(4);
-    expect(numIdenticalPairs([1,1,1,1])).toEqual(6);
-    expect(numIdenticalPairs([1,2,3])).toEqual(0);
+// test('Given an array of integers nums, return the number of good pairs', () => {
+//     expect(numIdenticalPairs([1,2,3,1,1,3])).toEqual(4);
+//     expect(numIdenticalPairs([1,1,1,1])).toEqual(6);
+//     expect(numIdenticalPairs([1,2,3])).toEqual(0);
+// });
+// test('Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.', () => {
+//     expect(finalValueAfterOperations(["--X","X++","X++"])).toEqual(1);
+//     expect(finalValueAfterOperations(["++X","++X","X++"])).toEqual(3);
+//     expect(finalValueAfterOperations(["X++","++X","--X","X--"])).toEqual(0);
+// });
+test('Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].', () => {
+    expect(shuffle([2,5,1,3,4,7], 3)).toEqual([2,3,5,4,1,7]);
+    expect(shuffle([1,2,3,4,4,3,2,1], 4)).toEqual([1,4,2,3,3,2,4,1]);
+    expect(shuffle([1,1,2,2], 2)).toEqual([1,2,1,2]);
 });
