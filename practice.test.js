4d1d87ef9842d2d709a587e32b8f446b62207baa
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2024-03-29 21:20:05 -0700

    second practice day with anna

--- practice.test.js
+++ practice.test.js
@@ -1,7 +1,15 @@
-const hammingWeight = require('./practice');
+// const hammingWeight = require('./practice');
 
-test('Write a function that takes the binary representation of a positive integer and returns the number of set bits it has (also known as the Hamming weight).', () => {
-    expect(hammingWeight(11)).toEqual(3);
-    expect(hammingWeight(128)).toEqual(1);
-    expect(hammingWeight(2147483645)).toEqual(30);
-});
+// test('Write a function that takes the binary representation of a positive integer and returns the number of set bits it has (also known as the Hamming weight).', () => {
+//     expect(hammingWeight(11)).toEqual(3);
+//     expect(hammingWeight(128)).toEqual(1);
+//     expect(hammingWeight(2147483645)).toEqual(30);
+// });
+
+const removeDuplicates = require('./practice');
+
+test('remove the duplicates in-place such that each unique element', ()=>{
+    expect(removeDuplicates([1, 1, 2])).toEqual(2);
+    expect(removeDuplicates([0,0,1,1,1,2,2,3,3,4])).toEqual(5);
+
+})
