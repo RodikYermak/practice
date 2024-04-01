6e430ffbe11b43a11ed5fefc6c5ebe945d9ba4da
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2024-03-31 20:42:02 -0700

    leetcode practice

--- practice.test.js
+++ practice.test.js
@@ -6,10 +6,18 @@
 //     expect(hammingWeight(2147483645)).toEqual(30);
 // });
 
-const removeDuplicates = require('./practice');
+// const removeDuplicates = require('./practice');
 
-test('remove the duplicates in-place such that each unique element', ()=>{
-    expect(removeDuplicates([1, 1, 2])).toEqual(2);
-    expect(removeDuplicates([0,0,1,1,1,2,2,3,3,4])).toEqual(5);
+// test('remove the duplicates in-place such that each unique element', ()=>{
+//     expect(removeDuplicates([1, 1, 2])).toEqual(2);
+//     expect(removeDuplicates([0,0,1,1,1,2,2,3,3,4])).toEqual(5);
 
-})
+// })
+
+const lengthOfLastWord = require('./practice');
+
+test('Given a string s consisting of words and spaces, return the length of the last word in the string.', () => {
+    expect(lengthOfLastWord('Hello World')).toEqual(5);
+    expect(lengthOfLastWord('   fly me   to   the moon  ')).toEqual(4);
+    expect(lengthOfLastWord('luffy is still joyboy')).toEqual(6);
+});
